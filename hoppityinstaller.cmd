331ebb7f9398a3ccb2cfffbbffc37ae5b0f62fe8
electric-otter <electric-otter-github@proton.me>
2025-03-28 09:47:08 -0700

    Update hoppityinstaller.cmd

--- hoppityinstaller.cmd
+++ hoppityinstaller.cmd
@@ -1,6 +1,15 @@
 :: Install Hoppity
 git clone https://gitlab.com/kryptonitewing/hoppity.git
-cd ..
 cd hoppity
+
+:: Run the Hoppity installer
 START /w execute.cmd
+
+:: Confirmation message
 echo Start hoppity installer
+
+:: Move hoppity to AlterGameStudio directory
+move /Y hoppity ..\AlterGameStudio
+
+:: Confirmation message after moving
+echo Hoppity has been successfully installed and moved to AlterGameStudio.
