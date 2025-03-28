33677d365e15ae12f5fd10008ae9d8daf3d522cb
electric-otter <electric-otter-github@proton.me>
2025-03-28 09:59:04 -0700

    Delete Samples directory

--- Samples/XnaGraphicsProfileChecker_4_0/XnaGraphicsProfileChecker/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-//-----------------------------------------------------------------------------
-// stdafx.cpp
-//
-// Microsoft XNA Community Game Platform
-// Copyright (C) Microsoft Corporation. All rights reserved.
-//-----------------------------------------------------------------------------
-
-#include "stdafx.h"
